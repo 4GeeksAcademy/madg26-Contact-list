11574850d05fb49e45b32943ab74a6dd574b212d
Madg26 <manux_852@hotmail.com>
2025-03-28 23:28:30 +0000

    [Proyecto finalizado]

--- src/pages/CreateContact.jsx
+++ src/pages/CreateContact.jsx
@@ -24,32 +24,32 @@ const CreateContact = () => {
       })
       .catch((err) => { return err })
   }
-  
+
 
   return (
     <div className="container text-center mt-5" id="Create">
       <p><strong>Full name</strong></p>
       <input className="input"
         value={newContact.name}
-        onChange={(e) => setNewContact({...newContact, name: e.target.value})}
+        onChange={(e) => setNewContact({ ...newContact, name: e.target.value })}
         type="text"
         placeholder="Enter name"></input>
       <p><strong>Phone</strong></p>
       <input className="input"
         value={newContact.phone}
-        onChange={(e) => {setNewContact({...newContact, phone: e.target.value})}}
-        type= "text"
+        onChange={(e) => { setNewContact({ ...newContact, phone: e.target.value }) }}
+        type="text"
         placeholder="Enter phone"></input>
       <p><strong>Email</strong></p>
       <input className="input"
-        value={newContact.email}
-        onChange={(e) => setNewContact({...newContact, email: e.target.value})}
         type="email"
+        value={newContact.email}
+        onChange={(e) => setNewContact({ ...newContact, email: e.target.value })}
         placeholder="Enter email"></input>
       <p><strong>Address</strong></p>
       <input className="input"
         value={newContact.address}
-        onChange={(e) => setNewContact({...newContact, address : e.target.value})}
+        onChange={(e) => setNewContact({ ...newContact, address: e.target.value })}
         type="text"
         placeholder="Enter address"></input>
 
--- src/pages/EditContact.jsx
+++ src/pages/EditContact.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import useGlobalReducer from '../hooks/useGlobalReducer'
 import { useNavigate, useParams } from 'react-router-dom'
 
 const EditContact = () => {
   const navigate = useNavigate()
   const { id } = useParams()
-  const { store, dispatch } = useGlobalReducer()
+  const { store } = useGlobalReducer()
   const singleId = store.listContact.find(contacts => contacts.id === parseInt(id))
 
   const [editar, setEditar] = useState({
-    name: singleId.name,
-    phone: singleId.phone,
-    email: singleId.email,
-    address: singleId.address
+    name: "",
+    phone: "",
+    email: "",
+    address: ""
   })
   function editContact() {
     fetch(`https://playground.4geeks.com/contact/agendas/madg26/contacts/${singleId.id}`, {
@@ -20,52 +20,55 @@ const EditContact = () => {
       body: JSON.stringify(editar),
       headers: { "Content-Type": "application/json" }
     })
-      .then((response) => { return response.json()})
-      .then((data)=>{
-        dispatch({         
-          type: "save_contacts",
-          payload: { contactos : data.contacts }
-        })
+      .then((response) => { return response.json() })
+      .then((data) => {
+        console.log(data)
+        navigate("/")
       })
-      .catch((err) => {return err})
+      .catch((err) => { return err })
   }
+  function handleSubmit(e) {
+    e.preventDefault();
+    editContact();
+    
+  }
+  useEffect(() => {
+    if (singleId !== undefined) {
 
-  console.log(singleId);
-
+      setEditar(singleId)
+    }
+  }, [singleId])
   return (
     <div className="container text-center mt-5" id="Create">
       <h1>Editar contacto</h1>
       <p><strong>Full name</strong></p>
-      <input className="input"
-        value={editar.name}
-        onChange={(e) => {
-          setEditar({ ...editar, name: e.target.value })
-        }}></input>
-      <p><strong>Phone</strong></p>
-      <input className="input"
-        value={editar.phone}
-        onChange={(e) => {
-          setEditar({ ...editar, phone: e.target.value })
-        }}></input>
-      <p><strong>Email</strong></p>
-      <input className="input"
-        value={editar.email}
-        onChange={(e) => {
-          setEditar({ ...editar, email: e.target.value })
-        }}></input>
-      <p><strong>Address</strong></p>
-      <input className="input"
-        value={editar.address}
-        onChange={(e) => {
-          setEditar({ ...editar, address: e.target.value })
-        }}></input>
-      <button className="btn btn-success mt-3"
-        onClick={() => {
-          return (
-            editContact(),
-            navigate("/")
-          )
-        }}>Guardar</button>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input className="input"
+          value={editar.name}
+          onChange={(e) => {
+            setEditar({ ...editar, name: e.target.value })
+          }}></input>
+        <p><strong>Phone</strong></p>
+        <input className="input"
+          value={editar.phone}
+          onChange={(e) => {
+            setEditar({ ...editar, phone: e.target.value })
+          }}></input>
+        <p><strong>Email</strong></p>
+        <input className="input"
+          value={editar.email}
+          onChange={(e) => {
+            setEditar({ ...editar, email: e.target.value })
+          }}></input>
+        <p><strong>Address</strong></p>
+        <input className="input"
+          value={editar.address}
+          onChange={(e) => {
+            setEditar({ ...editar, address: e.target.value })
+          }}></input>
+
+        <button className="btn btn-success mt-3" type="submit">Guardar</button>
+      </form>
     </div>
   )
 }
